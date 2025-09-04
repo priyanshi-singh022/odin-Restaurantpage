3f88c81d5c5a2d2cac539325022d22cee7576b95
pikupika <peeyuvi2002@gmail.com>
2025-09-05 01:03:38 +0530

    error handling

--- src/index.js
+++ src/index.js
@@ -1,25 +1,4 @@
 import "./styles.css";
-import vegesImg from "./Images/pasta-spaghetti-vegetables-spices-isolated-white.jpg"
-import tunadish from "./Images/tuna-salad.jpg"
 import pageLoad from "./pageload.js"
 
 pageLoad();
-
-
-// hero section 
-
-const img = document.createElement("img");
-img.src = vegesImg;
-img.alt = "Veges";
-img.classList.add("hero-img");
-document.getElementById("hero-sec").prepend(img);
-
-
-
-
-// menu 
-const tunaimg = document.createElement("img");
-tunaimg.src = tunadish;
-tunaimg.alt = "Tuna dish";
-tunaimg.classList.add("dish-img");
-document.getElementById("Contact").prepend(tunaim);
